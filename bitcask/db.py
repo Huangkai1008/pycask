ba5b5291035f11adce64568532fbc6d956549774
huangkai <h1770360848@outlook.com>
2020-12-02 22:24:38 +0800

    feat(storage): storage constructor

---
+++ bitcask/db.py
@@ -0,0 +1,115 @@
+from pathlib import Path
+from typing import Generator, Optional
+
+from bitcask.file import LogFile
+
+__all__ = ['DataStore']
+
+
+class DataStore:
+    """Implements the KV store on disk.
+
+    Attributes:
+        dir_name:
+            The directory of data storage.
+
+        key_dir:
+            A hash table that maps every key to an entry.
+
+        active_datafile:
+            The active data file accepts all write requests.
+
+    """
+
+    def __init__(self, dir_name: str) -> None:
+        self.dir_name: str = dir_name
+        self.key_dir: dict = dict()
+        self.active_datafile: Optional[LogFile] = None
+
+        # Initialise data directory.
+        self._init_data_dir()
+
+        # Initialise data files.
+        self._init_data_files()
+
+        # Initialise the key dir.
+        self._init_key_dir()
+
+    def get(self, key: str) -> Optional[str]:
+        """Retrieve a value by key from datastore.
+
+        If the key does not exist, return `None`.
+
+        Args:
+            key: The given key.
+
+        Returns:
+            The value if key exists, `None` otherwise.
+
+        """
+        ...
+
+    def put(self, key: str, value: str) -> None:
+        """Store a key and value in datastore.
+
+        Args:
+            key: The key to store.
+            value: The value to store.
+
+        """
+        ...
+
+    def delete(self, key: str) -> None:
+        """Delete a key from datastore.
+
+        If the key does not exist, do nothing.
+
+        Args:
+            key: The key to delete.
+
+        """
+        ...
+
+    def keys(self) -> Generator[str, None, None]:
+        """Generate all keys in datastore.
+
+        Yields: The next key in the range of all keys.
+
+        """
+        ...
+
+    def __getitem__(self, key: str) -> Optional[str]:
+        return self.get(key)
+
+    def __setitem__(self, key: str, value: str) -> None:
+        return self.put(key, value)
+
+    def __delitem__(self, key: str) -> None:
+        return self.delete(key)
+
+    def _init_data_dir(self) -> None:
+        """Initialise the data directory.
+
+        If the directory does not exist, recursively create all folders.
+
+        """
+        path = Path(self.dir_name)
+        if not path.exists():
+            path.mkdir(parents=True)
+
+    def _init_data_files(self) -> None:
+        """Initialise data files.
+
+        FIXME: Only one data file at first.
+
+        """
+        self.active_datafile = LogFile(self.dir_name)
+
+    def _init_key_dir(self) -> None:
+        """Initialise the key dir.
+
+        If the file exists, then the `key_dir` should be initialized with the
+        contents of the file read.
+
+        """
+        ...
